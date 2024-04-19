eeb3f370b2e95ff0a8f6c7a6e496cc0399d4b9ff
Shabihz <162735979+Shabihz@users.noreply.github.com>
2024-04-19 14:46:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env/node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+//Asking Question from Users through Inquirer
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//conditional statements If-Else
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Invalide Input");
+}
